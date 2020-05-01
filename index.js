4e387d90e3ea6eb9ebf259ac232a44075f9a2419
RocktimRajkumar <rajkumar.rocktim@gmail.com>
2020-05-01 17:18:20 +0530

    feat: docker added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('We Have successfully deploy our first nodejs application using jenkins!');
+  res.send('We have successfully deploy our first nodejs application using jenkins! and docker');
 });
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
